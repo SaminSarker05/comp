7d0822071abb4f0e84ffef619a0402ae82265c3b
Samin Sarker <saminsarker05@gmail.com>
2024-05-31 14:36:22 -0400

    a simpler solution added

--- 150/Intervals/easy/228.py
+++ 150/Intervals/easy/228.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
+
+        # SIMPLER
+        intervals = [] # 0. hold all intervals in nums
+        for num in nums:
+            # 1. if an interval exists check to see if current num can be a part it
+            if intervals and intervals[-1][1] == num - 1:
+                # 2. if valid update the range for the last interval
+                intervals[-1][1] = num
+            # 3. if invalid start a new interval and add to list
+            else: intervals.append([num, num])
+        
+        # 4. list comprehension for stirng formating
+        return [f'{x}->{y}' if x != y else f'{x}' for x, y in intervals]
+
+
         # 0. edge cases if len nums is 0 or 1
         if len(nums) == 0: return []
         if len(nums) == 1: return [str(nums[0])]
@@ -31,3 +46,4 @@ class Solution(object):
         else: res.append(str(running))
  
         return res
+        
