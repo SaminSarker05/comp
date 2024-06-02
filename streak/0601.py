0598a5bb3a86d000077961980a2c32e920f94de6
Samin Sarker <saminsarker05@gmail.com>
2024-06-01 20:25:57 -0400

    starting summer streak

---
+++ streak/0601.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+        # 0. use two pointers at opposite ends of stirng
+        while l < r:
+            # 1. swap the values at each index
+            s[l], s[r] = s[r], s[l]
+            # 2. increment pointers; stop loop when l <= r
+            l += 1
+            r -= 1
