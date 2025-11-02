b89c8d137016d3fadfccd9641d7ea954464f9487
Samin Sarker <saminsarker05@gmail.com>
2025-11-02 18:24:29 -0500

    sweep through grid, simulation, tle caused by traversal per guard

--- streak/1102.cpp
+++ streak/1102.cpp
@@ -1,45 +1,64 @@
-class Solution:
-    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        # simulation ? 
-        grid = [[0 for i in range(n)] for j in range(m)]
-        walls = set([(x, y) for x,y in walls])
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        std::vector<std::vector<int>> grid(m, std::vector<int>(n, 0));
+        // 0 unwatched, 1 guard, 2 wall, 3 watched
+        // do sweeps across the grid
 
-        # TLE
-        for p, q in guards:
-            x, y = p + 1, q
-            while (0 <= x < m and 0 <= y < n) and (x, y) not in walls:
-                grid[x][y] = -1
-                x += 1
-            x, y = p - 1, q
-            while (0 <= x < m and 0 <= y < n) and (x, y) not in walls:
-                grid[x][y] = -1
-                x -= 1
-            x, y = p, q + 1
-            while (0 <= x < m and 0 <= y < n) and (x, y) not in walls:
-                grid[x][y] = -1
-                y += 1
-            x, y = p, q - 1
-            while (0 <= x < m and 0 <= y < n) and (x, y) not in walls:
-                grid[x][y] = -1
-                y -= 1
-            
-            grid[p][q] = 1
-        
-        # for row in grid:
-        #     print(row)
-        
-        res = 0
+        // mark guards and walls
+        for (auto& g : guards) {
+            grid[g[0]][g[1]] = 1;
+        }
+        for (auto& w : walls) {
+            grid[w[0]][w[1]] = 2;
+        }
+        // left to right
+        for (int i = 0; i < m; i++) {
+            bool guard = false;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) { guard = true; } 
+                else if (grid[i][j] == 2) { guard = false; }
+                else if (guard) { grid[i][j] = 3; }
+            }
+        }
+        // right to left
+        for (int i = 0; i < m; i++) {
+            bool guard = false;
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 1) { guard = true; } 
+                else if (grid[i][j] == 2) { guard = false; }
+                else if (guard) { grid[i][j] = 3; }
+            }
+        }
+        // top to bottom
+        for (int j = 0; j < n; j++) {
+            bool guard = false;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 1) { guard = true; } 
+                else if (grid[i][j] == 2) { guard = false; }
+                else if (guard) { grid[i][j] = 3; }
+            }
+        }
+        // bottom to top
+        for (int j = 0; j < n; j++) {
+            bool guard = false;
+            for (int i = m - 1; i >= 0; i--) {
+                if (grid[i][j] == 1) { guard = true; } 
+                else if (grid[i][j] == 2) { guard = false; }
+                else if (guard) { grid[i][j] = 3; }
+            }
+        }
 
-        for i in range(m):
-            for j in range(n):
-                if (i, j) in walls: continue
-                if grid[i][j] == 0:
-                    res += 1
-        
-        return res
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    res++;
+                }
+            }
+        }
 
+        return res;
+    }
+};
 
-
-
-
-        
